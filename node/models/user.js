d2663f1096effed2e0576249cb82968568921e3f
SOunit <workbookwork7@gmail.com>
2020-12-27 22:25:13 +0900

    added cart add

--- node/models/user.js
+++ node/models/user.js
@@ -15,9 +15,26 @@ class User {
   }
 
   addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex((cp) => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new mongodb.ObjectID(product._id),
+        quantity: 1,
+      });
+    }
+
     const updatedCart = {
-      items: [{ productId: new mongodb.ObjectID(product._id), quantity: 1 }],
+      items: updatedCartItems,
     };
+
     const db = getDb();
     db.collection('users').updateOne(
       { _id: new mongodb.ObjectID(this._id) },
